043af04104b276a7c00a39b1e11e19d3cc5ced6e
Michael Rico <rico.michael@gmail.com>
2016-04-30 08:24:32 -0700

    (doc) Add db util folder to work on seed data

---
+++ util/dataGen.js
@@ -0,0 +1,60 @@
+module.export = (function() {
+
+  'use strict';
+
+  class User {
+    constructor(username, email, password) {
+      this.username = username;
+      this.email = email;
+      this.password = password;
+    }
+  }
+
+  var usernames = [
+    'happy_sad',
+    'haveahappyday',
+    'SomethingNew',
+    '5mileys',
+    'takes2long',
+    'w8t4u',
+    'askme',
+    'Bidwell',
+    'massdebater',
+    'iluvmen',
+    'yomatey',
+    'idontknow',
+    'likme',
+    'TurtleCat',
+    'BlogWobbles',
+    'LuckyDusty',
+    'RumChicken',
+    'StonedTime',
+    'HackySacky',
+    'VillageIdiots'
+  ];
+
+  var names = [
+    'Emma',
+    'Noah',
+    'Olivia',
+    'Liam',
+    'Sophia',
+    'Mason',
+    'Isabella',
+    'Jacob',
+    'Ava',
+    'William',
+    'Mia',
+    'Ethan',
+    'Emily',
+    'Michael',
+    'Abigail',
+    'Alexander',
+    'Madison',
+    'James',
+    'Charlotte',
+    'Daniel'
+    ];
+
+
+})();
